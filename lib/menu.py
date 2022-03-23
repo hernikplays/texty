5ff4eb2b4a9301a61f83302195c46b079cd65d40
Matyáš Caras <contact@hernikplays.cz>
2022-03-23 14:20:59 +0100

    Create menu.py

---
+++ lib/menu.py
@@ -0,0 +1,6 @@
+import keyboard
+
+class MenuManager: # TODO
+    def __init__(self,selections:list):
+        self.selected = 0 # current selection
+        self.selections = selections # available selections
